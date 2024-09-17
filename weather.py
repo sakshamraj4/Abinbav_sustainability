af213bf7c695bb0fbc7b9d688de8b7e3da219d6f
sakshamraj4 <saksham.raj@arya.ag>
2024-09-17 10:09:16 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -283,7 +283,7 @@ def create_activity_progress_plot():
         dict(Task='Pesticide Spray', Start='2024-08-09', Finish='2024-08-10', Done=8, NotDone=51, Status='done'),
         dict(Task='Fungicide Spray-3', Start='2024-09-06', Finish='2024-09-07', Done=1, NotDone=58, Status='done'),
         dict(Task='Harvesting', Start='2024-09-08', Finish='2024-09-17', Done=35, NotDone=24, Status='ongoing'),
-        dict(Task='Threshing', Start='2024-09-16', Finish='2024-09-17', Done=1, NotDone=58, Status='ongoing')
+        dict(Task='Threshing', Start='2024-09-16', Finish='2024-09-18', Done=1, NotDone=58, Status='ongoing')
     ]
     data_combined = []
     for item in data:
